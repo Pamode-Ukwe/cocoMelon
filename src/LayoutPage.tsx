a4fc49254503d0ba51fef9e6d90887fe671506e9
Pamode <ebenezerukwe@gmail.com>
2024-07-13 06:20:12 +0100

    first commit

---
+++ src/LayoutPage.tsx
@@ -0,0 +1,15 @@
+import Homepage from "./Homepage"
+import ProductList from "./ProductList"
+import Shop from "./Shop"
+
+const LayoutPage = () => {
+  return (
+    <>
+        <Homepage/>
+        <Shop/>
+        <ProductList/>
+    </>
+  )
+}
+
+export default LayoutPage
